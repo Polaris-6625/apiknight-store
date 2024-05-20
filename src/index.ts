c7745473e2843e8f1d034f1397c262282fcdc29c
liuyuyang <2281487673@qq.com>
2024-05-20 23:26:11 +0800

    feat: 增加了isDispatching判定

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,45 @@ import { Func, StoreType } from "./type";
 function createStore<T = any>(reducer: Func,initState?: T): StoreType<T> {
     let state: T | undefined = initState;
     let listeners: Func[] = [];
-
+    let isDispatching = false;
     function subscribe(callback: Func) {
+        if (isDispatching) {
+            throw new Error("Reducers may not dispatch actions.");
+        }
         listeners.push(callback);
     }
 
     function unSubscribe(func: Func) {
+        if (isDispatching) {
+            throw new Error("Reducers may not dispatch actions.");
+        }
         const index = listeners.indexOf(func);
         listeners.splice(index, 1);
     }
 
     function dispatch(action: any) {
-        state = reducer(state, action);
+        if (isDispatching) {
+            throw new Error("Reducers may not dispatch actions.");
+        }
+        try {
+            isDispatching = true;
+            state = reducer(state, action);
+        }
+        catch (error) {
+            throw error;
+        }
+        finally {
+            isDispatching = false;
+        }
         listeners?.forEach(listener => {
             listener();
         });
     }
 
     function getState() {
+        if (isDispatching) {
+            throw new Error("Reducers may not dispatch actions.");
+        }
         return state;
     }
 
